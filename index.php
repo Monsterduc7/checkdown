b30a34498aecbcbafaec2ef226f520817aa08f7f
Philipp Klug <Philipp Klug>
2016-09-04 22:00:01 +0200

    Umstellung von PDO auf MySql. Noch nicht unfktional

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
-  include "config.php";
+  include "inc/config.php";
+
+	
+
+
+	/* Sorting */
+	$sql = "SELECT * FROM roster";
+
+	if($_GET['sort'] == 'name')
+	{
+	    $sql .= " ORDER BY name";
+	}
+
+
 ?>
 <!doctype html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
@@ -21,9 +34,6 @@
                 padding-bottom: 20px;
             }
         </style>
-        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="css/main.css">
-        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
     <body>
 
@@ -32,6 +42,12 @@
 			<legend>Add Roster</legend>
 	   		<form name="AddRoster" action="index.php" method="POST" class="form-horizontal">
 	       		<div class="input-group">
+	       			<span class="input-group-addon" >Nr</span>
+					<input name="nr" type="text" class="form-control" placeholder="" aria-describedby="basic-addon1">
+
+					<span class="input-group-addon" >Position</span>
+					<input name="position" type="text" class="form-control" placeholder="" aria-describedby="basic-addon1">
+
 	       			<span class="input-group-addon" >Name</span>
 					<input name="name" type="text" class="form-control" placeholder="" aria-describedby="basic-addon1">
 
@@ -45,50 +61,109 @@
 					<input name="talent" type="text" class="form-control" placeholder="" aria-describedby="basic-addon1">
 
 					<span class="input-group-addon" >Salary</span>
-					<input name="salary" type="text" class="form-control" placeholder="" aria-describedby="basic-addon1">
+					<input name="sallery" type="text" class="form-control" placeholder="" aria-describedby="basic-addon1">
 
 					<span class="input-group-addon" >Contract Years</span>
 					<input name="contract" type="text" class="form-control" placeholder="" aria-describedby="basic-addon1">
 				</div>
-			    		<button name="SaveAddRoster" type="submit" class="btn btn-primary pull-right">Add to Roster</button>
+		    		<button name="SaveAddRoster" type="submit" class="btn btn-primary pull-right">Add to Roster</button>
 			</form>
 
+				<div class="btn-toolbar">
+				    <button class="btn btn-primary">New User</button>
+				    <button class="btn">Import</button>
+				    <button class="btn">Export</button>
+				</div>
+				<div class="well">
+				    <table class="table">
+				      <thead>
+				        <tr>
+				          <th>#</th>
+				          <th><a href="index.php?sort=name">Name</a></th>
+				          <th>Age</th>
+				          <th>Overall</th>
+				          <th style="width: 36px;"></th>
+				        </tr>
+				      </thead>
+				      <tbody>
+					      <?php
+							//$sql = "SELECT * FROM roster";
+					      echo $sql;
+							foreach ($dbh->query($sql) as $row) {
+								 echo '<tr>';
+								 echo '<td>' . $row["id"] . ' </td>';
+								 echo '<td>' . $row["name"] . ' </td>';
 
-			<?php
-				$sql = "SELECT * FROM roster";
- 
-				$db_erg = mysqli_query( $db_link, $sql );
-				if ( ! $db_erg )
-				{
-				  die('Ungültige Abfrage: ' . mysqli_error());
-				}
-
-			?>
+						         echo '<td>';
+						         echo '<a href="user.html"><i class="icon-pencil"></i></a>';
+						         echo '<a href="#myModal" role="button" data-toggle="modal"><i class="icon-remove"></i></a>';
+						          echo '</td>';
+						        echo '</tr>';
+							}
+							echo '</table>';
+						?>
 
 
 
     	</div> <!-- /container -->  
 
-    </body>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.js"></script>
-    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.js"><\/script>')</script>
-    <script src="js/vendor/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    </body>
 </html>
-
 <?php
-	
-	/* ####### ADD Roster ####### */
+/* ####### ADD Roster ####### */
 	if(isset($_POST['SaveAddRoster']))
 	{
-		$name = $_POST['name'];
-
-		/* ## PDO ## */
-		$sql= 'INSERT INTO roster(Name) VALUES ("'.$name.'")';
-		$stmt = $dbh->prepare($sql);
-		// $stmt->bindValue(':name', $name);
 		
-		$stmt->execute();
+		$nr = $_POST['nr'];
+		$position = $_POST['position'];
+		$name = $_POST['name'];
+		$age = $_POST['age'];
+		$overall = $_POST['overall'];
+		$talent = $_POST['talent'];
+		$sallery = $_POST['sallery'];
+		$contract = $_POST['contract'];
+		/*
+		echo 'nr: ' . $nr . '<br>';
+		echo 'position: ' . $position . '<br>';
+		echo 'name: ' . $name . '<br>';
+		echo 'age: ' . $age . '<br>';
+		echo 'overall: ' . $overall . '<br>';
+		*/
+		/* ## PDO ## */
+		//$sql= 'INSERT INTO roster(nr, position, name, age, overall, talent, sallery, contract) VALUES (":nr", ":position", ":name", ":age", ":overall", ":talent", ":sallery", ":contract")'; /*("'.$nr.','.$position.','.$name.','.$age.','.$overall.','.$talent.','.$sallery.','.$contract.'")*/
+		//$stmt = $dbh->prepare($sql);
+
+		/*$stmt->bindValue(':nr', $nr);
+		$stmt->bindValue(':position', $position);
+		$stmt->bindValue(':name', $name);
+		$stmt->bindValue(':age', $age);
+		$stmt->bindValue(':overall', $overall);
+		$stmt->bindValue(':talent', $talent);
+		$stmt->bindValue(':sallery', $sallery);
+		$stmt->bindValue(':contract', $contract);*/
+		
+		/*$stmt->execute(array(
+		    ":nr" => $nr,
+		    ":position" => $nr,
+		    ":name" => $name,
+		    ":age" => $age,
+		    ":talent" => $talent,
+		    ":sallery" => $sallery,
+		    ":contract" => $contract
+
+		));
+		echo $sql;*/
+		//$stmt->execute();
+
+		
+
+
+
+
 	}
 
 ?>
+
+
